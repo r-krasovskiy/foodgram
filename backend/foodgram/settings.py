8b723c1889e2e0445b8ebb091eb8b4abaf3ade04
Роман Красовский <krasovskiyroman@gmail.com>
2025-01-18 10:34:13 +0300

    settings correction

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-# DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+
 # Для развертывания на сервере:
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 # Для локального развертывания:
@@ -161,3 +161,7 @@ REST_FRAMEWORK = {
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://foodgram.ddnsfree.com'
+]
